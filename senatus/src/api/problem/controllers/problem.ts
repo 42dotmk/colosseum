00a030d341dbefe5f1c2edddecb06b81de8fb889
Ilija Boshkov <ilija@codechem.com>
2023-12-26 19:23:43 +0100

    Adding initial data model

---
+++ senatus/src/api/problem/controllers/problem.ts
@@ -0,0 +1,7 @@
+/**
+ * problem controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::problem.problem');
---
+++ senatus/src/api/problem/routes/problem.ts
@@ -0,0 +1,7 @@
+/**
+ * problem router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::problem.problem');
