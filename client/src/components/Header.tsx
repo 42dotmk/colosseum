514e57c507164d07a8c38b7d2762d17988b81b29
Veronika Gjoreva <veronika.gjoreva@gmail.com>
2024-01-19 17:28:36 +0100

    COLOS-8: Added a bottom border to the Header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,32 +1,47 @@
-import { Toolbar, Box, Typography, Button, AppBar } from '@mui/material';
+import { Toolbar, Box, Typography, Button, AppBar } from "@mui/material";
 
 const Header = () => {
   return (
     <Box flexGrow={1}>
-      <AppBar position="static" >
+      <AppBar position="static" sx={{ borderBottom: "1px solid grey" }}>
         <Toolbar sx={{ marginX: "50px" }}>
-
           <Box flexGrow={1} display={"inline-flex"}>
             <img src="/logo.svg" alt="logo" />
-            <Typography color={"primary"} marginX={1} variant="h6" textTransform={"uppercase"}>
+            <Typography
+              color={"primary"}
+              marginX={1}
+              variant="h6"
+              textTransform={"uppercase"}
+            >
               Colosseum
             </Typography>
           </Box>
 
           <Box>
-            <Button color="inherit" href='/'>Home</Button>
-            <Button color="inherit" href='/compete'>Compete</Button>
+            <Button color="inherit" href="/">
+              Home
+            </Button>
+            <Button color="inherit" href="/compete">
+              Compete
+            </Button>
           </Box>
 
-          <Box flexGrow={1} display={"flex"} justifyContent={"flex-end"} gap={1}>
-            <Button variant="outlined" color="inherit">Login</Button>
-            <Button variant="contained" color="primary">Sign up</Button>
+          <Box
+            flexGrow={1}
+            display={"flex"}
+            justifyContent={"flex-end"}
+            gap={1}
+          >
+            <Button variant="outlined" color="inherit">
+              Login
+            </Button>
+            <Button variant="contained" color="primary">
+              Sign up
+            </Button>
           </Box>
-
         </Toolbar>
       </AppBar>
     </Box>
-
   );
 };
 
