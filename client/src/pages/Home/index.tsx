7a56a7399320dc1ee6161a70927f8e1e2c46ca0e
Veronika Gjoreva <veronika.gjoreva@gmail.com>
2024-01-19 20:12:38 +0100

    COLOS-8: Stretch the Landing page width to full page

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import { Grid } from "@mui/material";
 const HomePage = () => {
   return (
     <Grid container>
-      <Grid item xs={9} height={"100vh"} width={"100vw"}>
+      <Grid item height={"100vh"} width={"100vw"}>
 
       </Grid>
     </Grid>
