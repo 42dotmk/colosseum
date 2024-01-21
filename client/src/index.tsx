e5233a40c36c2c9c2b2201ef7e01ce1d6a6e7e97
Veronika Gjoreva <veronika.gjoreva@gmail.com>
2024-01-21 15:58:54 +0100

    COLOS-5: Restore PR changes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,18 @@
+import { ThemeProvider } from "@emotion/react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import React from "react";
 
 import { store } from "./redux/store.ts";
+import themeOptions from "./theme/index.ts";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <React.StrictMode>
+      <ThemeProvider theme={themeOptions}>
         <App />
+      </ThemeProvider>
     </React.StrictMode>
   </Provider>
 );
