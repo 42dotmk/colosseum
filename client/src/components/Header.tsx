c8f2a65979fe7f3600d052b0bd1fa99a75f99b88
Veronika Gjoreva <veronika.gjoreva@gmail.com>
2024-02-13 18:07:07 +0100

    COLOS-15: Resolve wrong directory format

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -18,11 +18,11 @@ function Header() {
         position="static"
         sx={{ borderBottom: 1, borderColor: 'divider' }}
       >
-        <Toolbar sx={{ backgroundColor: "background.default" }}>
+        <Toolbar sx={{ backgroundColor: 'background.default' }}>
           <Link to="/">
-            <Box flexGrow={1} display={"inline-flex"} marginLeft={5}>
+            <Box flexGrow={1} display="inline-flex" marginLeft={5}>
               <img src="/logo.svg" alt="logo" />
-              <Typography color={"primary"} variant="h6" textTransform={"uppercase"} marginX={1} >
+              <Typography color="primary" variant="h6" textTransform="uppercase" marginX={1}>
                 Colosseum
               </Typography>
             </Box>
