a06e457f41b109a60dd21658a188533f73f5f6a9
Ilija Boshkov <ilija@codechem.com>
2024-04-01 15:59:16 +0200

    Fix C# build output display

--- executioner/runtimes/languages/csharp/entrypoint.sh
+++ executioner/runtimes/languages/csharp/entrypoint.sh
@@ -12,7 +12,7 @@ for inputFile in "${files[@]}"; do
   echo "output/$filename.stdout"
   # If there's an error in $ERROR pipe it into the stderr
   if [ $ERROR != 0 ]; then
-    echo "Compilation error:\\n $ERROR" > "output/$filename.stderr"
+    echo "Compilation error:\\n $BUILD_OUTPUT" > "output/$filename.stderr"
     continue;
   fi
   { time (cat $inputFile | timeout ${TIMEOUT} ./out/csharp 1> "output/$filename.stdout" 2> "output/$filename.stderr") ; } 2> "output/$filename.time"
