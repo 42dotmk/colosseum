d4d31dd7a08571a19da876a1dc83f483e2646a62
Veronika Gjoreva <veronika@halicea.com>
2024-06-30 13:54:35 +0200

    COLOS-25: Setup the Submissions section UI

---
+++ client/src/pages/Compete/Submissions/SubmissionPreview.tsx
@@ -0,0 +1,84 @@
+import { Typography, Button, Box } from "@mui/material";
+import CodeEditor from "../../../components/CodeEditor";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import { useEffect, useState } from 'react';
+
+type Props = {
+  language: string;
+  code: string[];
+};
+
+const SubmissionPreview = ({ language, code }: Props) => {
+  const [copyButton, setCopyButton] = useState({
+    label: "COPY CODE",
+    icon: <ContentCopyIcon fontSize="small" sx={{ marginRight: 1 }} />,
+  })
+
+  useEffect(() => {
+    setCopyButton({
+      label: "COPY CODE",
+      icon: <ContentCopyIcon fontSize="small" sx={{ marginRight: 1 }} />,
+    });
+  }, [code])
+  
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(code.join("\n"));
+    setCopyButton({
+      label: "COPIED",
+      icon: <DoneAllIcon fontSize="small" sx={{ marginRight: 1, color: "green" }} />,
+    });
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "100%",
+        gap: 2,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          color="text.secondary"
+          variant="subtitle1"
+          align="left"
+          fontWeight="bold"
+        >
+          {language}
+        </Typography>
+          <Button
+            variant="outlined"
+            size="medium"
+            onClick={copyToClipboard}
+            sx={{
+              border: 1,
+              borderRadius: 1.5,
+              borderColor: "text.disabled",
+              color: copyButton.label === 'COPIED' ? "green" : "text.secondary",
+              "&:hover": {
+                border: 1,
+                borderRadius: 1.5,
+                bgcolor: "text.disabled",
+                color: copyButton.label === 'COPIED' ? "green" : "primary.main",
+              }
+            }}
+          >
+            {copyButton.icon} {copyButton.label}
+          </Button>
+      </Box>
+
+      <CodeEditor language={language.toLowerCase()} code={code} readOnly />
+    </Box>
+  );
+};
+export default SubmissionPreview;
